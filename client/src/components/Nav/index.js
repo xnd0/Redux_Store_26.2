8ce4cd965661a79d3d74869c9e8b71a199fc84a9
xndcmd <xndcmd@gmail.com>
2022-08-17 14:42:35 -0700

    adjusted styling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -43,8 +43,7 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          The xnd0 Custom eCommerce Shop
         </Link>
       </h1>
 
